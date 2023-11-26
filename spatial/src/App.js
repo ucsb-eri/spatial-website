e7966afd0cf2ecd7d9b2636c26abd4991b5b0377
danwill <daniel.willett2@gmail.com>
2023-11-26 15:51:05 -0800

    added material ui blog template as starting point and bega n customizing home screen. also added an Overview and upcoming events components

--- spatial/src/App.js
+++ spatial/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './templates/Home'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
+  const THEME = createTheme({
+    typography: {
+     "fontFamily": `Avenir, "Century Gothic", sans-serif`,
+     "fontSize": 14,
+     "fontWeightLight": 100,
+     "fontWeightRegular": 400,
+     "fontWeightMedium": 500
+    }
+ });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={THEME}>
+      <div className="App">
+        <Home />
+      </div>
+    </ThemeProvider>
+    
   );
 }
 

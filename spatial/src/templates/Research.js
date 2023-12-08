6727d69740bb67c02b03f4723740b320f26852e6
danwill <daniel.willett2@gmail.com>
2023-12-08 09:43:46 -0800

    added a Research page, but still need to fill it out

---
+++ spatial/src/templates/Research.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+function Research() {
+
+    return (
+        <Container>
+            <Typography>Groups..?</Typography>
+            <Typography>Initiatives..?</Typography>
+        </Container>
+    )
+}
+
+export default Research

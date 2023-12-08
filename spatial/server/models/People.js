6fcd6947ed8e5573c7ee66f17368d1496930fb24
danwill <daniel.willett2@gmail.com>
2023-12-08 13:19:21 -0800

    added server side template and moved react files into client side folder

---
+++ spatial/server/models/People.js
@@ -0,0 +1,91 @@
+const { Schema, model } = require('mongoose');
+
+const peopleSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  image: {
+    type: String, // will store this on locally in an uploads folder
+    required: false,
+    unique: false,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: [50, 'Description must be at least 50 characters'],
+    maxlength: [5000, 'Description cannot exceed 500 characters'],
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['Faculty', 'Staff', 'Graduate Student', 'Undergrad Student']
+  },
+  current: {
+    type: Boolean,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  location: {
+    type: String,
+    required: false,
+    unique: false,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: false,
+    unique: false,
+    trim: true,
+    match: [/^\+\d{1,2}-\d{3}-\d{3}-\d{4}$/, 'Must match the format xxx-xxx-xxxx']
+  },
+  gscholar: {
+    type: String,
+    required: false,
+    unique: false,
+    trim: true,
+    match: [
+      /^(ftp|http|https):\/\/[^ "]+$/,
+      'Must be a valid URL format (e.g., http://example.com)',
+    ],
+  },
+  website: {
+    label: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: false,
+      trim: true,
+      match: [
+        /^(ftp|http|https):\/\/[^ "]+$/,
+        'Must be a valid URL format (e.g., http://example.com)',
+      ],
+    },
+  }
+});
+
+const People = model('People', peopleSchema);
+
+module.exports = People;
---
+++ spatial/server/models/index.js
@@ -0,0 +1,5 @@
+const People = require('./People');
+const AdminProfile = require('./AdminProfile')
+const Projects = require('./Projects')
+
+module.exports = { People, AdminProfile, Projects };

dd6f4e39b9c3c1b4f201d810ac969bbfd7271c01
danwillett <115373509+danwillett@users.noreply.github.com>
2024-02-14 09:36:39 -0800

    updated seed.js with admin seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,6 +1,7 @@
 const db = require('../config/connection');
 const { People, Projects, AdminProfile } = require('../models');
 const peopleSeeds = require('./peopleSeeds.json');
+const adminSeeds = require('./adminSeed.json')
 
 db.once('open', async () => {
 
@@ -9,9 +10,21 @@ db.once('open', async () => {
     await People.collection.drop();
     await People.create(peopleSeeds);
 
+
     console.log('all done!');
-    process.exit(0);
   } catch (err) {
     throw err;
   }
+
+  try {
+    await AdminProfile.deleteMany({});
+    await AdminProfile.collection.drop();
+    await AdminProfile.create(adminSeeds)
+
+    console.log("got the admins!")
+  } catch (err) {
+    throw err
+  }
+
+  process.exit(0);
 });
